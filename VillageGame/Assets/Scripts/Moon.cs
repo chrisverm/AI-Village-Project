6e5cb1d8840c26903a6118e585e5a36533c961b0
chrisverm <chrisverm@comcast.net>
2014-03-19 20:52:54 -0400

    Weather States
    The beginning of different weather conditions with transitionts between
    them.

---
+++ VillageGame/Assets/Scripts/Moon.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moon : MonoBehaviour
+{
+	public Conditions currCondition, prevCondition;
+	public ConditionColors currColors, prevColors;
+	public GameObject moon;
+	public GameObject aura;
+	public Light dirLight;
+	private float transition;
+	private float transitionTime;
+	private Color moonColor, auraColor, lightColor;
+	
+	void Start () { }
+	
+	public void ChangeState(Conditions newCondition, ConditionColors newColors, float timeForTransition)
+	{
+		prevCondition = currCondition;
+		prevColors = currColors;
+		
+		currCondition = newCondition;
+		currColors = newColors;
+		
+		transition = 0.0f;
+		transitionTime = timeForTransition;
+	}
+	
+	public void SetState(Conditions newCondition, ConditionColors newColors)
+	{
+		ChangeState(newCondition, newColors, 0);
+		
+		moonColor = currColors.MOON;
+		auraColor = currColors.AURA;
+		lightColor = currColors.LIGHT;
+		
+		moon.renderer.material.color = moonColor;
+		aura.renderer.material.color = auraColor;
+		dirLight.color = lightColor;
+	}
+	
+	void Update ()
+	{
+		aura.transform.LookAt(GameManager.Instance.mayor.Position);
+		aura.transform.Rotate(new Vector3(90, 0, 0));
+		
+		if (transition < transitionTime)
+		{
+			moonColor = Color.Lerp(prevColors.MOON, currColors.MOON, transition / transitionTime);
+			auraColor = Color.Lerp(prevColors.AURA, currColors.AURA, transition / transitionTime);
+			lightColor = Color.Lerp(prevColors.LIGHT, currColors.LIGHT, transition / transitionTime);
+			transition += Time.deltaTime;
+			
+			moon.renderer.material.color = moonColor;
+			aura.renderer.material.color = auraColor;
+			dirLight.color = lightColor;
+		}
+	}
+}
--- VillageGame/Assets/Scripts/Weather.cs
+++ VillageGame/Assets/Scripts/Weather.cs
@@ -3,48 +3,49 @@ using System.Collections;
 
 public enum Conditions
 {
-	FULL_MOON,
-	BLOOD_MOON,
-	NEW_MOON,
+	FULL_MOON = 0,
+	BLOOD_MOON = 1,
+	NEW_MOON = 2,
+}
+
+[System.Serializable]
+public class ConditionColors
+{
+	public Color MOON;
+	public Color AURA;
+	public Color LIGHT;
+	public float AuraAlpha;
+	
+	public void Initialize()
+	{
+		AURA.a = AuraAlpha;
+	}
 }
 
 public class Weather : MonoBehaviour
 {
-	public GameObject moon;
-	public GameObject moonAura;
-	public Light moonLight;
-
-	Conditions condition;
+	private Conditions condition;
+	public ConditionColors fullMoon;
+	public ConditionColors bloodMoon;
+	public ConditionColors newMoon;
+	public Moon moon;
+	public float transitionTime;
 
 	// Use this for initialization
 	void Start ()
 	{
+		fullMoon.Initialize();
+		bloodMoon.Initialize();
+		newMoon.Initialize();
+		
 		condition = Conditions.FULL_MOON;
+		moon.SetState(condition, fullMoon);
+		moon.ChangeState(Conditions.NEW_MOON, newMoon, transitionTime);
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
-		moonAura.transform.LookAt(GameManager.Instance.mayor.Position);
-		moonAura.transform.Rotate(new Vector3(90, 0, 0));
 
-		switch (condition)
-		{
-		case Conditions.FULL_MOON:
-			moon.renderer.material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-			moonAura.transform.renderer.material.color = new Color(1.0f, 1.0f, 1.0f, 1.25f);
-			moonLight.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
-			break;
-		case Conditions.BLOOD_MOON:
-			moon.renderer.material.color = new Color(1.0f, 0, 0, 0.5f);
-			moonAura.transform.renderer.material.color = new Color(1.0f, 0.2f, 0.2f, 1.75f);
-			moonLight.color = new Color(1.0f, 0.1f, 0.1f, 1.0f);
-			break;
-		case Conditions.NEW_MOON:
-			moon.renderer.material.color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
-			moonAura.transform.renderer.material.color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
-			moonLight.color = new Color(0.5f, 0.5f, 0.5f, 1.0f);
-			break;
-		}
 	}
 }
