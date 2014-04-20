8ab8617466042db0ce59ecedb5d3e78f4539fa7a
ArcadiusRex <waw5228@rit.edu>
2014-04-20 18:59:25 -0400

    Added the Basis for DecisionTree Shifts

--- VillageGame/Assets/Scripts/Managers.cs
+++ VillageGame/Assets/Scripts/Managers.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Managers : SingletonMonoBehaviour<Managers>
 {
@@ -8,10 +9,15 @@ public class Managers : SingletonMonoBehaviour<Managers>
 	private static EntityManager entityManager;
 	private static SpawningManager spawnManager;
 
+	private static Dictionary<string, DecisionTree> decDictionary;
+
 	public static GameManager Game { get { return gameManager; } }
 	public static WeatherManager Weather { get { return weatherManager; } }
 	public static EntityManager Entity { get { return entityManager; } }
 	public static SpawningManager Spawn { get { return spawnManager; } }
+
+	public static Dictionary<string, DecisionTree> DecDictionary { get { return decDictionary; } }
+
 
 	// Use this for initialization
 	void Start ()
@@ -20,5 +26,17 @@ public class Managers : SingletonMonoBehaviour<Managers>
 		weatherManager = (WeatherManager)FindObjectOfType(typeof(WeatherManager));
 		entityManager = (EntityManager)FindObjectOfType(typeof(EntityManager));
 		spawnManager = (SpawningManager)FindObjectOfType(typeof(SpawningManager));
+
+		LoadDecisionTrees ();
+	}
+
+	void LoadDecisionTrees()
+	{
+		decDictionary.Add("v00", new DecisionTree("Assets/Resources/VillagerDecisionTree.txt"));
+		//decDictionary.Add("v01", new DecisionTree("Assets/Resources/VillagerDecisionTreeBlood.txt"));
+		//decDictionary.Add("v02", new DecisionTree("Assets/Resources/VillagerDecisionTreeNew.txt"));
+		//decDictionary.Add("w00", new DecisionTree("Assets/Resources/VillagerDecisionTree.txt"));
+		//decDictionary.Add("w01", new DecisionTree("Assets/Resources/VillagerDecisionTreeBlood.txt"));
+		//decDictionary.Add("w02", new DecisionTree("Assets/Resources/VillagerDecisionTreeNew.txt"));
 	}
 }
